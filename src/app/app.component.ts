408d7287c7fa5fcb20dcd355454779d052ab6a3d
mikeestonia <mikk.visnap@gmail.com>
2022-05-18 13:59:40 +0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class AppComponent {
   name = 'Angular addsd';
   output;
+  discountRate;
 
   profileForm = new FormGroup({
     numItems: new FormControl(1),
@@ -17,10 +18,51 @@ export class AppComponent {
   });
 
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-    this.output =
+    let taxRate = this.getTaxRate(this.profileForm.controls.state.value);
+
+    let priceTimeItems =
       this.profileForm.controls.numItems.value *
       this.profileForm.controls.price.value;
+    this.discountRate = this.getDiscount(priceTimeItems);
+    let discountedPrice =
+      priceTimeItems - priceTimeItems * (this.discountRate / 100);
+    this.output = discountedPrice * (taxRate / 100) + discountedPrice;
     console.warn(this.profileForm.value);
+  }
+
+  getDiscount(total: number) {
+    if (total >= 50000) {
+      return 15;
+    }
+    if (total >= 10000) {
+      return 10;
+    }
+    if (total >= 7000) {
+      return 7;
+    }
+    if (total >= 5000) {
+      return 5;
+    }
+    if (total >= 1000) {
+      return 3;
+    }
+    return 0;
+  }
+
+  getTaxRate(state: string) {
+    switch (state) {
+      case 'UT':
+        return 6.85;
+      case 'NV':
+        return 8;
+      case 'TX':
+        return 6.25;
+      case 'AL':
+        return 4;
+      case 'CA':
+        return 8.25;
+      default:
+        return 0;
+    }
   }
 }
